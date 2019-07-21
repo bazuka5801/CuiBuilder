449a03895adf5eabbcaaf3ea9a05e13262955e01
StokesMagee <stokesgamingmc@gmail.com>
2019-07-21 03:25:58 -0400

    Added: Creating a new object now creates one in the selected object

--- Assets/C#/CuiManager.cs
+++ Assets/C#/CuiManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
@@ -18,18 +18,39 @@ public class CuiManager : MonoBehaviour
     private void Update()
     {
         if (Input.GetKeyDown( KeyCode.X ))
-        {
-            var obj = CreateInternal();
-            var rTransform = obj.GetComponent<RectTransformComponent>();
-            rTransform.SetPixelLocalPosition( new Vector2( 100, 100 ) );
-            rTransform.SetPixelSize( new Vector2( 100, 100 ) );
+            CreateInternalParent();
+    }
+
+    private CUIObject CreateInternalParent() {
+        var obj = PoolManager.Get(PrefabType.Cui);
+
+        GameObject selectedObj = null;
+        try {
+            var selected = CUIObject.Selection; //Get the currently selected object, this throws errors if the selection is HUD/OVERLAY
+            selectedObj = HierarchyView.GetCurrent().Where(selected.Contains).Select(p => p.gameObject).ToList()[0];
+        } catch (Exception) {
+
         }
+
+        var rTransform = obj.GetComponent<RectTransformComponent>();
+        if (selectedObj != null) {
+            obj.transform.SetParent(selectedObj.transform, false);
+            Hierarchy.Lookup[ obj ].SetParent( Hierarchy.Lookup[ selectedObj ] );
+            rTransform.SetPixelLocalPosition(new Vector2(0, 0));
+        } else {
+            obj.transform.SetParent(m_RootDefault.transform, false);
+            Hierarchy.Lookup[ obj ].SetParent( Hierarchy.Lookup[ m_RootDefault ] );
+            rTransform.SetPixelLocalPosition(new Vector2(100, 100));
+        }
+        rTransform.SetPixelSize(new Vector2(100, 100));
+
+        return CUIObject.Lookup[ obj ];
     }
 
     private CUIObject CreateInternal()
     {
-        var obj = PoolManager.Get( PrefabType.Cui );
-        obj.transform.SetParent( m_RootDefault.transform, false );
+        var obj = PoolManager.Get(PrefabType.Cui);
+        obj.transform.SetParent(m_RootDefault.transform, false);
         Hierarchy.Lookup[ obj ].SetParent( Hierarchy.Lookup[ m_RootDefault ] );
         return CUIObject.Lookup[ obj ];
     }
